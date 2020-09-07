3b510ee4e0a87b993d5086eaadec1063cf6ef429
Demián Vselko <54943405+demianvselko@users.noreply.github.com>
2020-09-07 19:05:59 -0300

    App React

---
+++ src/components/Task.js
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+
+class Task extends Component {
+    styleCompleted(){
+        return {
+            fontSize: '20px',
+            color: this.props.task.done ? 'gray': 'black',
+            textDecoration: this.props.task.done ? 'line-through': 'none'
+        }
+    }
+
+    render(){
+        const {task} = this.props;
+        return <p style={this.styleCompleted()}>
+                {task.title} - 
+                {task.description} -
+                {task.done} -
+                {task.id}
+            <input type="checkbox" onChange={this.props.checkDone.bind(this, task.id)}></input>
+            <button style={btnDelete} onClick={this.props.deleteTask.bind(this, task.id)}>
+                x
+            </button>
+        </p>
+    }
+}
+
+Task.propTypes={
+     task: PropTypes.object.isRequired
+}
+const btnDelete = {
+
+    fontSize: '18px',
+    background: '#ea2027',
+    color: '#fff',
+    padding:'10px 15px',
+    border: 'none',
+    borderRadius:'50%',
+    cursor:'pointer',
+}
+
+export default Task;
